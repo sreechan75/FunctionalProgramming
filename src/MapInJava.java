1c4ce71c2dbeeefe12b6a54fd9c742cfcb81df80
sreechan-aws <106415919+sreechan-aws@users.noreply.github.com>
2023-09-05 22:28:11 -0700

    initial commit.

---
+++ src/MapInJava.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MapInJava {
+    public static void main(String[] args) {
+        Integer [] intArray = {1,2,3,4,5,6,7,8,9,10};
+        List<Integer> listOfIntegers = Arrays.asList(intArray);
+
+        Function<Integer, Integer> timesTwo = (x) -> x * 2;
+
+        List<Integer> doubled = listOfIntegers
+                .stream()
+                .map(timesTwo)
+                .collect(Collectors.toList());
+
+        System.out.println(doubled);
+    }
+}
